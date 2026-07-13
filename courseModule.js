44543242d7a86e989086cd75cde10dc01cff3dd5
PicksN1k <csjohnson2838@gmail.com>
2026-07-13 19:52:01 -0400

    modified:   courseModule.js

--- courseModule.js
+++ courseModule.js
@@ -7,18 +7,20 @@ console.log("Read", courseData.courses.length, "courses");
 export const lookupByCourseId =  (id) => {
 	console.log("\nLookup by CourseId", id);
 	let result;
-	let re = new RegExp(id);
+	// fill the code
 
-	// Fill in the code
+	let pattern = new RegExp(id);
+	result = courseData.courses.filter(course => pattern.test(course.course_id));
 
 	return result;
 };
 
 export const lookupByCourseName= (name) => {
   console.log("\nLookup by CourseName", name);
-  let result;
+	let result;
+	// fill the code
 	
-	// Fill in the code
+	result = courseData.courses.filter(course => course.course_name.toLowerCase().includes(name.toLowerCase()));
 	
 	return result;
 };
@@ -26,9 +28,10 @@ export const lookupByCourseName= (name) => {
 export const getRandomCourse = () => {
 	console.log("\nA Random Course");
 	let result;
+	// fill the code
 	
-	// Fill in the code
-	
+	let index = Math.floor(Math.random() * courseData.courses.length);
+	result = courseData.courses[index];
+
 	return result;
 };
-
